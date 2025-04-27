95799fb9e63be44cb0f895c47b2a80549c97d926
vkkononenko <konichforever@gmail.com>
2025-04-27 14:37:59 +0200

    InitialCommit

---
+++ template/src/ModConfig.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace QM_Template
+{
+    public class ModConfig
+    {
+        public short AmmoStackSize = 100;
+        public short TrashStackSize = 100;
+        public short GrenadeStackSize = 100;
+        public short ConsumableStackSize = 100;
+        public short RepairStackSize = 100;
+        public short PlaceableStackSize = 100;
+        public short FixationStackSize = 100;
+        public short DeviceStackSize = 100;
+        public static ModConfig LoadConfig(string configPath)
+        {
+            ModConfig config;
+
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+            };
+
+            if (File.Exists(configPath))
+            {
+                try
+                {
+                    string sourceJson = File.ReadAllText(configPath);
+
+                    config = JsonConvert.DeserializeObject<ModConfig>(sourceJson, serializerSettings);
+
+                    //Add any new elements that have been added since the last mod version the user had.
+                    string upgradeConfig = JsonConvert.SerializeObject(config, serializerSettings);
+
+                    if (upgradeConfig != sourceJson)
+                    {
+                        Debug.Log("Updating config with missing elements");
+                        Debug.Log(upgradeConfig);
+                        //re-write
+                        File.WriteAllText(configPath, upgradeConfig);
+                    }
+
+
+                    return config;
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError("Error parsing configuration.  Ignoring config file and using defaults");
+                    Debug.LogException(ex);
+
+                    //Not overwriting in case the user just made a typo.
+                    config = new ModConfig();
+                    return config;
+                }
+            }
+            else
+            {
+                config = new ModConfig();
+
+                string json = JsonConvert.SerializeObject(config, serializerSettings);
+                File.WriteAllText(configPath, json);
+
+                return config;
+            }
+        }
+    }
+}
---
+++ template/src/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using MGSC;
+using UnityEngine;
+
+namespace QM_Template
+{
+    public static class Plugin
+    {
+        public static string ModAssemblyName => Assembly.GetExecutingAssembly().GetName().Name;
+        public static string ConfigPath => Path.Combine(Application.persistentDataPath, ModAssemblyName, "config.json");
+        public static string ModPersistenceFolder => Path.Combine(Application.persistentDataPath, ModAssemblyName);
+        public static ModConfig Config { get; private set; }
+
+        [Hook(ModHookType.AfterConfigsLoaded)]
+        public static void AfterConfig(IModContext context)
+        {
+            Directory.CreateDirectory(ModPersistenceFolder);
+            Config = ModConfig.LoadConfig(ConfigPath);
+            new Harmony("$UserName$_" + ModAssemblyName).PatchAll();
+
+            foreach (BasePickupItemRecord record in Data.Items.Records)
+            {
+                AmmoRecord ammoRecord = (record as CompositeItemRecord)?.GetRecord<AmmoRecord>();
+                TrashRecord trashRecord = (record as CompositeItemRecord)?.GetRecord<TrashRecord>();
+                ConsumableRecord consumableRecord = (record as CompositeItemRecord)?.GetRecord<ConsumableRecord>();
+                PlaceableDeviceRecord PlaceableDeviceRecord = (record as CompositeItemRecord)?.GetRecord<PlaceableDeviceRecord>();
+                FixationMedicineRecord FixationMedicineRecord = (record as CompositeItemRecord)?.GetRecord<FixationMedicineRecord>();
+                DeviceRecord DeviceRecord = (record as CompositeItemRecord)?.GetRecord<DeviceRecord>();
+                GrenadeRecord grenadeRecord = (record as CompositeItemRecord)?.GetRecord<GrenadeRecord>();
+                RepairRecord repairRecord = (record as CompositeItemRecord)?.GetRecord<RepairRecord>();
+                
+                if (ammoRecord != null) ammoRecord.MaxStack = Config.AmmoStackSize;
+                if (trashRecord != null) trashRecord.MaxStack = Config.TrashStackSize;
+                if (consumableRecord != null) consumableRecord.MaxStack = Config.ConsumableStackSize;
+                if (PlaceableDeviceRecord != null) PlaceableDeviceRecord.MaxStack = Config.PlaceableStackSize;
+                if (FixationMedicineRecord != null) FixationMedicineRecord.MaxStack = Config.FixationStackSize;
+                if (DeviceRecord != null) DeviceRecord.MaxStack = Config.DeviceStackSize;
+                if (grenadeRecord != null) grenadeRecord.MaxStack = Config.GrenadeStackSize;
+                if (repairRecord != null) repairRecord.MaxStack = Config.RepairStackSize;
+            }
+        }
+    }
+}
